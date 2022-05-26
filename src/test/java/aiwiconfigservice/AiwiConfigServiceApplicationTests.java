3b8345c56882e41faf877a24d00b8751a6118b3f
Del Castillo Martin <dcmartin@minsait.com>
2022-05-26 09:25:35 +0200

    Init commit

---
+++ src/test/java/aiwiconfigservice/AiwiConfigServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package aiwiconfigservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiwiConfigServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
